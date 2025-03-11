015a2f56508d4267d2019117f95454fee7f80c5d
Tarik El Hajjam <takkofo@gmail.com>
2025-03-11 00:57:27 +0000

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
      <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
+      <Button>Click me!</Button>
     </div>
     </>
   )
