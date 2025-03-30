4fadb37928ec91f749e8a54a566f466fd7305592
Tarik El Hajjam <takkofo@gmail.com>
2025-03-30 00:15:49 +0000

    fixed update task

--- src/components/board/board-task/board-task.tsx
+++ src/components/board/board-task/board-task.tsx
@@ -1,25 +1,46 @@
 import TaskEntity from "@/domain/board-entities";
 import { useState } from "react";
+import UpdateTask from "../tasks/update-task/update-task";
 
 interface taskProps {
-  getTask : (task : TaskEntity) => void;
+  getTask: (task: TaskEntity) => void;
   task: TaskEntity;
+  getUpdateTask: (task: TaskEntity) => void;
 }
 
-
-
-function BoardTask({getTask,task}: taskProps) {
+function BoardTask({ getTask, task, getUpdateTask }: taskProps) {
   const [taskDrag, setTaskDrag] = useState(task);
+  const [isOpen, setIsOpen] = useState(false);
+
+   const getUpdated = (updatedTask: TaskEntity)=>{    
+    getUpdateTask(updatedTask);
+  }
   const handleDrag = (currentTask: TaskEntity) => {
     setTaskDrag(currentTask);
-    getTask(taskDrag)
+    getTask(taskDrag);
   };
+  const handleUpdate = (e:any) => {
+    e.preventDefault();
+
+    setIsOpen(true)
+  }
   return (
-    <div className="items-center rounded-md border p-4 border-t-8 border-t-cyan-500 mt-6 cursor-pointer hover:bg-gray-200 duration-300 ease-in-out" data-item={task} draggable onDragStart={e=>handleDrag(task)} >
-      <div className="flex-1 space-y-1">
-        <p className="text-sm font-medium leading-none">{task.title}</p>
-        <p className="text-sm text-muted-foreground">{task.assigne}</p>
+    <div>
+
+      <div
+        className="items-center rounded-md border p-4 border-t-8 border-t-cyan-500 mt-6 cursor-pointer hover:bg-gray-200 duration-300 ease-in-out"
+        data-item={task}
+        draggable
+        onDragStart={() => handleDrag(task)}
+        onClick={handleUpdate}
+      >
+        <div className="flex-1 space-y-1">
+          <p className="text-sm font-medium leading-none">{task.title}</p>
+          <p className="text-sm text-muted-foreground">{task.assigne}</p>
+        </div>
       </div>
+      <UpdateTask task={task} isOpen={isOpen} setDialogOpen={setIsOpen} taskUpdate={getUpdated} />
+
     </div>
   );
 }
---
+++ src/components/board/tasks/update-task/update-task.tsx
@@ -0,0 +1,95 @@
+import TaskEntity from "@/domain/board-entities";
+import {
+  DialogHeader,
+  DialogFooter,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectValue,
+} from "@/components/ui/select";
+import { SelectTrigger } from "@radix-ui/react-select";
+import { Input } from "@/components/ui/input";
+import { boards } from "../../board-container/board-container";
+import { useState } from "react";
+
+function UpdateTask({ task, isOpen, setDialogOpen, taskUpdate }: 
+                    { task: TaskEntity, isOpen : boolean, 
+                      setDialogOpen:  (open:boolean) => void, 
+                      taskUpdate:(task:TaskEntity)=>void}) {
+  const [editTask, setTask] = useState<TaskEntity>(task);
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    taskUpdate(editTask);
+   };
+  const handleOnChange = (e: any) => {
+    const value = e.target.value;
+    setTask((prev) => ({
+      ...prev,
+      title: value,
+    }));
+  };
+
+  const handleOnSelect = (value: string) => {
+    setTask((prev) => ({
+      ...prev,
+      boardId: Number.parseInt(value),
+    }));
+  };
+  return (
+    <Dialog open={isOpen} onOpenChange={setDialogOpen}>
+        
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>Update Task</DialogTitle>
+          <DialogDescription></DialogDescription>
+        </DialogHeader>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col space-y-4 py-4">
+            <div className="grid flex-1 gap-2">
+              <Input
+                type="text"
+                placeholder="Task name"
+                 name="title"
+                onChange={handleOnChange}
+              />
+            </div>
+            <div className="grid flex-1 gap-2">
+              <Select
+                name="boardId"
+                value={editTask.boardId?.toString() ?? "1"}
+                onValueChange={handleOnSelect}
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="Select board" />
+                </SelectTrigger>
+                <SelectContent>
+                  {boards.map((board) => (
+                    <SelectItem key={board.id} value={board.id.toString()}>
+                      {board.title}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+          <DialogFooter className="sm:justify-start">
+            <DialogClose asChild>
+
+            <Button type="submit" onClick={() => setDialogOpen(false)}>Submit</Button>
+            </DialogClose>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default UpdateTask
